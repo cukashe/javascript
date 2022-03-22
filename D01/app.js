879dad1670eb68661d3b1f7d510267fd98a2f693
Abdirahman <cukashabdirahman4@gmail.com>
2022-03-22 12:44:08 +0300

    aasigment

---
+++ D01/app.js
@@ -0,0 +1,51 @@
+/**
+ * Variables - a container for data (identifier)
+ */
+// declariing a variable
+var username = "abdi";
+let Lastname = "mohammed";
+const PI= 3.142
+//const firstname =0;
+
+//assigning to variable
+username= "abdi"
+Lastname= "mohammed"
+
+// use variablees
+alert(username);
+console.log(username)
+
+/**
+ * Rules of naming variables
+ * variables can only contain letters, numbers, underscores or dollar sign
+ * Even though they can contaiin numbers variables cannot start with numbers
+ * variable are case sensitive
+ * you cannot re declare a variable
+ * variables should not have spaces
+ * variables should not be javascript keyword
+ */
+// DATA TYPES
+/**
+ * strings - textual content
+ */
+//let name="Ariana"
+
+/**
+ * number --numerical data
+ */
+//let age=56;
+/**
+ * boolean --something that evaluates to true or false
+ */
+let is_Raining=true;
+let is_auth=false;
+
+/**
+ * Declare relevant variables according to the following scenario
+ * john juma of age 45, is a new student at ist doing a software development course.Their high score 89.87 and his nickname is johnny
+ */
+var name="juma";
+var age="45";
+var course="Sftware Development";
+let score="89.87";
+let nickname="johnny"
