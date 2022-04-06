412847a925e8e1192f683f85664dfbe753f377ef
cukashe <64214133+cukashe@users.noreply.github.com>
2022-04-06 11:13:04 +0300

    day 09

--- D08/app.js
+++ D08/app.js
@@ -37,7 +37,50 @@ Greetings ("ann" , "hello")
 
 // return statement
 function add (a, b){
+    // result of running this function
     return a + b
 }
+let sum = add(4,5);
+console.log(sum);
 
-add (4,5)
+//example 2
+function checkAge(age) {
+    if (age > 18){
+    return true
+    }else{
+        return false;
+    }
+}
+// infinity is a number
+// -Infinity
+checkAge(Infinity)
+
+//function expression
+let  sayHello = function (name) {
+    alert (name);
+    
+}
+sayHello();
+
+//arrow functions 
+function add (a, b){
+    return a + b
+}
+const addtwo = (a,b) => a+b;
+
+const checkAge(age){
+    if (age > 18){
+    return true
+    }else{
+        return false;
+    }
+}
+const checkAge3 =(age) => (age > 18 ? true : false);
+
+function Greetings1(from , text) {
+    alert ("${text} ${from}");
+}
+Greetings ("ann" , "hello")
+
+const Greetings2 = (from , text) =>  alert ("${text} ${from}"); 
+
---
+++ D09/app.js
@@ -0,0 +1,102 @@
+// objects
+// key valued pairs(oop)
+// how to declear an object
+let user = {
+    name : 'lamech mongare',
+    email: 'lamechcruze@gmail.com',
+    age:30,
+    likes:['cooding',['eating'] ,['reading novels'] ,'learning']
+}
+console.log(user)
+console.log(user.likes[3])
+// declearing an object name student with properties studentid an couure set to 782 and fullstack respectivley
+let student={
+    studentId : 782,
+    course : 'fullStack',
+
+}
+// the use of square bracket notation 
+
+console.log(student)
+// changing a value
+student.studentId =3445
+// deleting a property
+delete student.studentId
+//how to access propertie of an object
+
+console.log(student.studentId)
+ 
+let john ={
+    fullName:'john the greatest',
+    userName:'john1234',
+    phoneNo:'123456789',
+    login:function(){
+        console.log('has logged in')
+
+    },
+    login2:() =>{
+        console.log('has logged in')
+
+    },
+    singUp(){
+        console.log('Has Logged In')
+    }
+}
+john.singUp()
+// creat an object called animal that has a method named jump which logs on the console i can jump
+let animal ={
+    method :'jumped',
+    phoneNo:'123456789',
+
+    logIn(){
+        console.log( this.method + 'I can jump')
+
+
+    },
+    sendMessage(){
+        console.log('message sent to')
+    }
+}
+animal.logIn()
+// This key word is irustrated above
+
+
+let users = {
+    name:'john',
+    surname :'smith'
+}
+console.log(users)
+users.name ='peter'
+delete users.name
+// example 2
+
+let salaries ={
+    john:100,
+    ann:160,
+    pete:130,
+}
+// the use for in loop
+let total =0
+for(const name in salaries){
+total+=salaries[name]// finding  the total
+console.log(name)
+console.log(salaries[name])//to  show the figures
+}
+console.log(total)
+
+
+
+
+function multiplyNumerical(){
+    
+    let measurment ={
+        height: 400,
+        width: 500,}
+    for(let measrment in height ){
+        if(measurment()[multiplyNumerical]== ' 500'){
+            height[multiplyNumerical]*=2
+            
+        }
+    }
+}
+multiplyNumerical()
